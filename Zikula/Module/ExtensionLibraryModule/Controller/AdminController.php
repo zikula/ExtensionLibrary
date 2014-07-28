23aff15e34f4487478a152421428d9392a647c23
cmfcmf <cmfcmf.flach@gmail.com>
2014-07-28 16:56:53 +0200

    Integration of push access check in admin interface as well as correct TimeZone setting.

--- Zikula/Module/ExtensionLibraryModule/Controller/AdminController.php
+++ Zikula/Module/ExtensionLibraryModule/Controller/AdminController.php
@@ -56,7 +56,7 @@ class AdminController extends \Zikula_AbstractController
         $rate = $rate['rate'];
 
         $now = new \DateTime('now');
-        $reset = \DateTime::createFromFormat('U', $rate['reset']);
+        $reset = \DateTime::createFromFormat('U', $rate['reset'], new \DateTimeZone('UTC'));
         $rate['minutesUntilReset'] = $now->diff($reset)->format('%i');
 
         $this->view->assign('rate', $rate);
@@ -65,6 +65,7 @@ class AdminController extends \Zikula_AbstractController
         if (!ImageManager::checkStorageDir(false)) {
             $this->view->assign('storageDir', ImageManager::STORAGE_PATH);
         }
+        $this->view->assign('hasPushAccess', Util::hasGitHubClientPushAccess($client));
 
         $this->view->assign('settings', $this->getVars());
 
