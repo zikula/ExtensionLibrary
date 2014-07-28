2d05b176c9ef9f8192fe84df40e46d4613106afc
Craig Heydenburg <craigh@mac.com>
2014-07-28 10:55:19 -0400

    add some pretty icons

--- Zikula/Module/ExtensionLibraryModule/Resources/views/plugins/modifier.elReleaseStateToAlert.php
+++ Zikula/Module/ExtensionLibraryModule/Resources/views/plugins/modifier.elReleaseStateToAlert.php
@@ -28,10 +28,10 @@ function smarty_modifier_elReleaseStateToAlert($state)
         case \Zikula\Module\ExtensionLibraryModule\Entity\CoreReleaseEntity::STATE_SUPPORTED:
             return "";
         case \Zikula\Module\ExtensionLibraryModule\Entity\CoreReleaseEntity::STATE_OUTDATED:
-            return "<div class=\"alert alert-warning\">" . __("You are about to download an OUTDATED and no longer supported core version. It does not receive bug fixes or maintenance any longer. Please use one of the supported versions instead.") . "</div>";
+            return "<div class=\"alert alert-warning\"><i class='fa fa-ban fa-3x pull-left'></i> " . __("You are about to download an OUTDATED and no longer supported core version. It does not receive bug fixes or maintenance any longer. Please use one of the supported versions instead.") . "</div>";
         case \Zikula\Module\ExtensionLibraryModule\Entity\CoreReleaseEntity::STATE_PRERELEASE:
-            return "<div class=\"alert alert-danger\">" . __("This core version is a pre-release only. NEVER use it on production sites. If you like to help, we invite you to test this version and report bugs.") . "</div>";
+            return "<div class=\"alert alert-danger\"><i class='fa fa-warning fa-3x pull-left'></i> " . __("This core version is a pre-release only. NEVER use it on production sites. If you like to help, we invite you to test this version and report bugs.") . "</div>";
         case \Zikula\Module\ExtensionLibraryModule\Entity\CoreReleaseEntity::STATE_DEVELOPMENT:
-            return "<div class=\"alert alert-danger\">" . __("DANGER: This is an in-development build. NEVER use it on production sites. It can likely be broken and absolutely not working. Really.") . "</div>";
+            return "<div class=\"alert alert-danger\"><i class='fa fa-warning fa-3x pull-left'></i> " . __("DANGER: This is an in-development build. NEVER use it on production sites. It can likely be broken and absolutely not working. Really.") . "</div>";
     }
 }
