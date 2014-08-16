1b6171c995342e04df83ceaee9ac490c5d3b50a2
Craig Heydenburg <craigh@mac.com>
2014-08-16 14:04:08 -0400

    update coreCompatibility on core type select

--- Zikula/Module/ExtensionLibraryModule/Resources/public/js/Zikula.ExtensionLibrary.User.AddExtension.js
+++ Zikula/Module/ExtensionLibraryModule/Resources/public/js/Zikula.ExtensionLibrary.User.AddExtension.js
@@ -15,8 +15,10 @@ jQuery(document).ready(function() {
         var apitype = jQuery('#el-add-extension-extension-apitype').val();
         if (apitype != '1.3') {
             jQuery('#el-add-extension-extension-namespace-input').show(400).find('input').attr('required', '');
+            jQuery('#el-add-extension-extension-coreCompatibility').val('>=1.4 <1.5');
         } else {
             jQuery('#el-add-extension-extension-namespace-input').hide(400).find('input').removeAttr('required');
+            jQuery('#el-add-extension-extension-coreCompatibility').val('>=1.3.5 <1.5');
         }
     }
 });
